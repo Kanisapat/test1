268c67bb48e5f116d0ae8097e0f2f12f1ac7be27
Kanisapat <67771827+Kanisapat@users.noreply.github.com>
2023-08-12 14:39:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let name = document.getElementById("name");
 let saynameBtn = document.getElementById("sayName");
 let changeBg = document.getElementById("changeBg");
+let Body = document.querySelector("Body");
 
 saynameBtn.addEventListener("click", () => {
     name.innerHTML = "HELLO ITS ME KAN";
+})
+
+changeBg.addEventListener("click", () => {
+    body.style.backgroundColor = "yellow*;
 })
