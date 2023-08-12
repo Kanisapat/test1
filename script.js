836a443ce2df305a46120086d3f1294d504a9ec0
Kanisapat <67771827+Kanisapat@users.noreply.github.com>
2023-08-12 14:27:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = document.getElementById("name");
+let saynameBtn = document.getElementById("sayName");
+let changeBg = document.getElementById("changeBg");
+
+saynameBtn.addEventListener("click" () => {
+    name.innerHTML = "hello";
+})
