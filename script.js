316b697389c62c0871fb1697134620127cedbdac
Kanisapat <67771827+Kanisapat@users.noreply.github.com>
2023-08-12 14:46:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let name = document.getElementById("name");
 let saynameBtn = document.getElementById("sayName");
 let changeBg = document.getElementById("changeBg");
-let body = document.querySelector("Body");
+let body = document.querySelector("body");
+let resetBtn = document.getElementById("reset");
 
 saynameBtn.addEventListener("click", () => {
     name.innerHTML = "HELLO ITS ME KAN";
@@ -9,4 +10,8 @@ saynameBtn.addEventListener("click", () => {
 
 changeBg.addEventListener("click", () => {
     body.style.backgroundColor = "yellow";
+})
+
+resetBtn.addEventListener("click", () => {
+    body.style.backgroundColor = "white";
 })
