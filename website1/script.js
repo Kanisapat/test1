52277df6a95e73b9d7130b1d276845d4b02b16b9
Kanisapat <67771827+Kanisapat@users.noreply.github.com>
2023-12-26 16:39:21 +0700

    Update script.js

--- website1/script.js
+++ website1/script.js
@@ -1,1 +1,12 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+  var audio = document.getElementById('backgroundAudio');
+
+  // Autoplay audio when the page is loaded
+  audio.autoplay = true;
+
+  // Loop the audio
+  audio.loop = true;
+
+  // You can add more controls or interactions using JavaScript
+});
